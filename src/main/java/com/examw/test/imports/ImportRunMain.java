d987fa582cf89d37865a2f6e566b7f29b7678fe7
jeasonyoung <jeason1914@qq.com>
2014-08-25 21:52:56 +0800

    程序初始化

---
+++ src/main/java/com/examw/test/imports/ImportRunMain.java
@@ -0,0 +1,19 @@
+package com.examw.test.imports;
+
+/**
+ * 试卷批量导入运行主类
+ * @author yangyong
+ * @since 2014年8月25日
+ */
+public class ImportRunMain {
+
+	/**
+	 * 运行入口类。
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello word!");
+	}
+
+}
